17ab79e93a3a680ce389b2351b0c06bcede28c37
Nathan Faucett <nathanfaucett@gmail.com>
2021-05-07 16:04:32 -0400

    katex import fix

--- src/lib/plugins/latex.ts
+++ src/lib/plugins/latex.ts
@@ -1,10 +1,10 @@
 import type { IPlugin } from '@aicacia/markdown';
 import { addPlugin } from '@aicacia/markdown';
-import { renderToString } from 'katex';
+import katex from 'katex';
 
 const latex: IPlugin = (code: string) => {
 	try {
-		return renderToString(code);
+		return katex.renderToString(code);
 	} catch (error) {
 		return error;
 	}

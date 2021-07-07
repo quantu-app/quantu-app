e2a0a372693667ef1025fc1e1b9ec2d580c5dddd
Nathan Faucett <nathanfaucett@gmail.com>
2021-07-07 17:51:27 -0400

    fix month stuff

--- src/lib/Books/JournalList.svelte
+++ src/lib/Books/JournalList.svelte
@@ -6,24 +6,20 @@
 	export let books: [UUID, IBookMeta][];
 	export let createOnDelete: (id: string, book: IBookMeta) => () => void;
 
-	$: booksByMonth = books.reduce<Record<string, [UUID, IBookMeta][]>>(
-		(booksByMonth, [id, book]) => {
+	$: booksByMonth = Object.entries(
+		books.reduce<Record<string, [UUID, IBookMeta][]>>((booksByMonth, [id, book]) => {
 			const date = new Date(book.createdAt),
-				monthDate = new Date(0);
+				month = new Date(date).toLocaleString('en-us', { month: 'short', year: 'numeric' });
 
-			monthDate.setFullYear(date.getFullYear());
-			monthDate.setMonth(date.getMonth());
+			const monthBooks = booksByMonth[month] || (booksByMonth[month] = []);
 
-			const monthDateJSON = monthDate.toJSON(),
-				monthBooks = booksByMonth[monthDateJSON] || (booksByMonth[monthDateJSON] = []);
 			monthBooks.push([id, book]);
 			return booksByMonth;
-		},
-		{}
+		}, {})
 	);
 </script>
 
-{#each Object.entries(booksByMonth) as [month, books]}
-	<h2>{new Date(month).toLocaleString('en-us', { month: 'short', year: 'numeric' })}</h2>
+{#each booksByMonth as [month, books]}
+	<h2>{month}</h2>
 	<DefaultList {books} {createOnDelete} />
 {/each}

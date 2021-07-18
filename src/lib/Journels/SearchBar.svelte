db6a3b81e87c71fe6cf73a4c530fecb07cac67f5
Nathan Faucett <nathanfaucett@gmail.com>
2021-07-18 15:23:26 -0400

    add forms

--- src/lib/Journels/SearchBar.svelte
+++ src/lib/Journels/SearchBar.svelte
@@ -28,33 +28,35 @@
 	}
 </script>
 
-<div class="input-group mt-2">
-	<input
-		type="text"
-		class="form-control"
-		placeholder="New Name (Leave blank to use current Date)"
-		aria-label="New Name (Leave blank to use current Date)"
-		required
-		bind:value={journelName}
-	/>
-	<button
-		type="submit"
-		disabled={journelCreating}
-		class="btn btn-primary"
-		aria-label="Create"
-		on:click={onCreateJournel}
-	>
-		{#if journelCreating}
-			<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
-		{/if}
-		Create
-	</button>
-</div>
-<div class="input-group mt-2 mb-4">
-	<input
-		type="text"
-		class="form-control"
-		placeholder="Filter by name"
-		bind:value={journelNameFilter}
-	/>
-</div>
+<form on:submit|preventDefault>
+	<div class="input-group mt-2">
+		<input
+			type="text"
+			class="form-control"
+			placeholder="New Name (Leave blank to use current Date)"
+			aria-label="New Name (Leave blank to use current Date)"
+			required
+			bind:value={journelName}
+		/>
+		<button
+			type="submit"
+			disabled={journelCreating}
+			class="btn btn-primary"
+			aria-label="Create"
+			on:click={onCreateJournel}
+		>
+			{#if journelCreating}
+				<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" />
+			{/if}
+			Create
+		</button>
+	</div>
+	<div class="input-group mt-2 mb-4">
+		<input
+			type="text"
+			class="form-control"
+			placeholder="Filter by name"
+			bind:value={journelNameFilter}
+		/>
+	</div>
+</form>

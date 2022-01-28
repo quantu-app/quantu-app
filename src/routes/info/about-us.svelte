28c9f5790c38b1991b51427f005299c0dfc5f851
John Faucett <jwaterfaucett@gmail.com>
2022-01-29 00:36:50 +0100

    Set $container-max-widths to 960 if screen >= lg, minor format tweak

--- src/routes/info/about-us.svelte
+++ src/routes/info/about-us.svelte
@@ -10,59 +10,61 @@
 <AppLayout>
 	<div class="container">
 		<div class="row py-4 my-4">
-			<h2>Our Vision</h2>
+			<div class="col">
+				<h2>Our Vision</h2>
 
-			<p>
-				The vision for QuantU is to provide the world's best learning platform for life-long
-				learners.
-			</p>
+				<p>
+					The vision for QuantU is to provide the world's best learning platform for life-long
+					learners.
+				</p>
 
-			<p>
-				We are primarly focused on the quantitative disciplines and sciences, especially
-				mathematics. Right now, mainly due to limited resources, we are focusing exclusively on
-				mathematics, but given enough community support and involvement we would love to eventually
-				be able to expand to close scientific fields such as physics, finance or computer science
-				and perhaps one day even further.
-			</p>
-			<p>
-				Central to our learning philosophy is the idea of <strong>life-long learning</strong>. We
-				believe that not only in order to succeed in the 21st century, but simply to be able to
-				explore and enjoy the beauty of knowledge in the universe, learning is something that should
-				not be done once in high school or university, and then left to be forgotten, but rather
-				something that each individual does on a daily basis, slowly increasing their understanding
-				of the world, improving their skills, and enhancing their ability to navigate complex
-				problems they encounter.
-			</p>
-			<p>
-				A further pillar in our vision is <strong>research backed optimized learning</strong> that is,
-				learning done in such a way that you forget as little as possible and waste as little time as
-				possible while gaining deep understanding. Learning should build knowledge and understanding
-				that becomes integrated with your abilities such that you can apply whatever you have learned
-				to your own projects, research or other endeavors in the real-world.
-			</p>
-			<p>
-				Learning should also be <strong>fun and enjoyable</strong>, like playing sports or video
-				games or music.
-			</p>
-			<p>
-				We want to build a community of like-minded people who love learning and creating high
-				quality learning content and we want to be able to provide life-long learners across the
-				globe with affordable and easy access to these educational resources while simultaneously
-				providing incredible content creators with a means for sustaining themselves while doing
-				what they love.
-			</p>
-			<p>
-				To be honest, right now we don't know how the monetary machinary can best work with our
-				ideas. We know we need funds, in-so-far as it enables us to build a better product and
-				dedicate more resources and time to it. Beyond this, our only goals are to build a
-				transparent and distributed organization such that QuantU revenues are community owned and
-				equitably distributed in the form of increases in earnings for creators and as cheaper
-				prices for life-long learners across the globe.
-			</p>
-			<p>
-				These are our goals for the moment and we ask that you join us not only in achieving them,
-				but in shaping their evolution into the future.
-			</p>
+				<p>
+					We are primarly focused on the quantitative disciplines and sciences, especially
+					mathematics. Right now, mainly due to limited resources, we are focusing exclusively on
+					mathematics, but given enough community support and involvement we would love to
+					eventually be able to expand to close scientific fields such as physics, finance or
+					computer science and perhaps one day even further.
+				</p>
+				<p>
+					Central to our learning philosophy is the idea of <strong>life-long learning</strong>. We
+					believe that not only in order to succeed in the 21st century, but simply to be able to
+					explore and enjoy the beauty of knowledge in the universe, learning is something that
+					should not be done once in high school or university, and then left to be forgotten, but
+					rather something that each individual does on a daily basis, slowly increasing their
+					understanding of the world, improving their skills, and enhancing their ability to
+					navigate complex problems they encounter.
+				</p>
+				<p>
+					A further pillar in our vision is <strong>research backed optimized learning</strong> that
+					is, learning done in such a way that you forget as little as possible and waste as little time
+					as possible while gaining deep understanding. Learning should build knowledge and understanding
+					that becomes integrated with your abilities such that you can apply whatever you have learned
+					to your own projects, research or other endeavors in the real-world.
+				</p>
+				<p>
+					Learning should also be <strong>fun and enjoyable</strong>, like playing sports or video
+					games or music.
+				</p>
+				<p>
+					We want to build a community of like-minded people who love learning and creating high
+					quality learning content and we want to be able to provide life-long learners across the
+					globe with affordable and easy access to these educational resources while simultaneously
+					providing incredible content creators with a means for sustaining themselves while doing
+					what they love.
+				</p>
+				<p>
+					To be honest, right now we don't know how the monetary machinary can best work with our
+					ideas. We know we need funds, in-so-far as it enables us to build a better product and
+					dedicate more resources and time to it. Beyond this, our only goals are to build a
+					transparent and distributed organization such that QuantU revenues are community owned and
+					equitably distributed in the form of increases in earnings for creators and as cheaper
+					prices for life-long learners across the globe.
+				</p>
+				<p>
+					These are our goals for the moment and we ask that you join us not only in achieving them,
+					but in shaping their evolution into the future.
+				</p>
+			</div>
 		</div>
 	</div></AppLayout
 >

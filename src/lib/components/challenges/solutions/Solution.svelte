d694e3f9b59ed4ea6f1a45c61bd5ee6dff6ad80e
Nathan Faucett <nathanfaucett@gmail.com>
2022-05-12 07:54:01 -0400

    edit on solution page

--- src/lib/components/challenges/solutions/Solution.svelte
+++ src/lib/components/challenges/solutions/Solution.svelte
@@ -7,11 +7,32 @@
 	import RichViewer from '$lib/components/editor/RichViewer.svelte';
 	import Challenge from '$lib/components/questions/Challenge.svelte';
 	import type { StateChallenge } from '$lib/state/challenges';
-	import type { StateChallengeSolution } from '$lib/state/challengeSolutions';
-	import { commentsTree, createComment } from '$lib/state/comments';
+	import {
+		updateChallengeSolution,
+		type StateChallengeSolution
+	} from '$lib/state/challengeSolutions';
+	import { createComment } from '$lib/state/comments';
+	import { currentUser } from '$lib/state/user';
 
 	export let challenge: StateChallenge;
 	export let solution: StateChallengeSolution;
+
+	let editing = false;
+	let updating = false;
+	function toggleEdit() {
+		editing = !editing;
+	}
+	async function onUpdate() {
+		updating = true;
+		try {
+			await updateChallengeSolution(challenge.department.url, challenge.url, solution.id, {
+				content: solution.solution
+			});
+		} finally {
+			updating = false;
+			editing = false;
+		}
+	}
 
 	let comment: any[] = [];
 	let commenting = false;
@@ -42,14 +63,38 @@
 <hr />
 
 <div>
-	<RichViewer bind:value={solution.solution} />
+	{#if editing}
+		<RichEditor bind:value={solution.solution} showHelper placeholder="Type comment..." />
+	{:else}
+		<RichViewer bind:value={solution.solution} />
+	{/if}
 </div>
 
 <div class="d-flex justify-content-between align-items-end">
 	<h3 class="mt-4 mb-0 pb-0">Comments</h3>
-	{#if !replying}
-		<a role="button" class="link-dark" on:click={toggleReply}>Reply to Solution</a>
-	{/if}
+	<div>
+		{#if !replying && !editing}
+			<a role="button" class="link-dark" on:click={toggleReply}>Reply to Solution</a>
+		{/if}
+		{#if editing && !replying}
+			<div class="btn-group" role="group">
+				<button class="btn btn-sm btn-primary" disabled={updating} on:click={onUpdate}
+					>Update</button
+				>
+				<button class="btn btn-sm btn-danger" disabled={updating} on:click={toggleEdit}
+					>Cancel</button
+				>
+			</div>
+		{/if}
+		{#if !editing && !replying && $currentUser?.id === solution.user.id}
+			<div class="btn-group" role="group">
+				<!-- <button class="btn btn-sm btn-danger" disabled={deletingComment} on:click={onDeleteComment}
+					>Delete</button
+				> -->
+				<button class="btn btn-sm btn-primary" on:click={toggleEdit}>Edit</button>
+			</div>
+		{/if}
+	</div>
 </div>
 
 <hr class="mt-0" />

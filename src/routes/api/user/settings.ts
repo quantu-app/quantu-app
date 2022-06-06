18586b8c0963a8c286c6b85017e6d542ae8cd97f
Nathan Faucett <nathanfaucett@gmail.com>
2022-06-06 08:01:28 -0400

    fix errors

--- src/routes/api/user/settings.ts
+++ src/routes/api/user/settings.ts
@@ -1,30 +1,29 @@
 import { authenticated } from '$lib/api/auth';
 import { run } from '$lib/prisma';
 
-export const patch = authenticated(
-	(event) =>
-		event.request.json().then((data) =>
-			run((client) =>
-				client.applicationSettings.upsert({
-					create: {
-						...data,
-						userId: event.locals.token.userId
-					},
-					update: {
-						...data,
-						userId: event.locals.token.userId
-					},
-					where: {
-						userId: event.locals.token.userId
-					}
-				})
-			).then((settings) =>
-				settings
-					? {
-							status: 200,
-							body: settings
-					  }
-					: { status: 404 }
-			)
-		))
+export const patch = authenticated((event) =>
+	event.request.json().then((data) =>
+		run((client) =>
+			client.applicationSettings.upsert({
+				create: {
+					...data,
+					userId: event.locals.token.userId
+				},
+				update: {
+					...data,
+					userId: event.locals.token.userId
+				},
+				where: {
+					userId: event.locals.token.userId
+				}
+			})
+		).then((settings) =>
+			settings
+				? {
+						status: 200,
+						body: settings
+				  }
+				: { status: 404 }
+		)
+	)
 );

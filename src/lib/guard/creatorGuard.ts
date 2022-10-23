7cc102e4d53048393dcc20d66b958c3d30a14478
Nathan Faucett <nathanfaucett@gmail.com>
2022-10-23 16:39:34 -0400

    fix creator guard

--- src/lib/guard/creatorGuard.ts
+++ src/lib/guard/creatorGuard.ts
@@ -1,7 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 
-export const creatorGuard = async ({ locals }: any) => {
-	const user = locals.user;
+export const creatorGuard = async ({ parent }: any) => {
+	const { user } = await parent();
+
 	if (!user?.creator) {
 		throw redirect(302, '/');
 	}
